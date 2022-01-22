f97d515a1d540b80b1b100fed85350e7a629101a
Daniel hogg <daniel300v@gmail.co.uk>
2022-01-22 16:16:55 +0000

    Co-authored-by: Fishy <fishykins@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut app = App::new();
+
+    app.add_plugins(DefaultPlugins);
+
+    app.add_startup_system(hello_world);
+
+    app.run();
+}
+
+
+fn hello_world() {
+    println!("hello world!");
 }
